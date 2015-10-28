ed99eae0bbaf4c9bfb00ae7261dcfbade53562be
Andrea Ganduglia <info@frequenze.it>
2015-10-28 18:55:17 +0100

    Banner added

--- tpl/homepage.php
+++ tpl/homepage.php
@@ -8,12 +8,13 @@
 
 <div style="text-align:center">
 	<p>DPR:<?=$app->data['dpr_x']?> | Screen: <span id="screen"></span></p>
-	<hr/>
 	<p><a href="/spool/image25_w640.jpg"><img src="/spool/image25_w640.jpg" width="640px" id="image_a"/></a></p>
 	<p id="rel_image_a"></p>
 	<p><a href="/spool/image26_w640.jpg"><img src="/spool/image26_w640.jpg" width="640px" id="image_b"/></a></p>
 	<p id="rel_image_b"></p>
 </div>
+
+<a href="https://github.com/andreaganduglia/dpr-detector"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
 
 <script>
 
